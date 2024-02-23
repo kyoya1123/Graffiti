92a67ad30e1054daed23ab96ee97ce7f9f25bee8
kyoya1123 <kyoryu32@gmail.com>
2024-02-23 19:12:34 +0900

    refactor

--- Graffiti.swiftpm/ContentView.swift
+++ Graffiti.swiftpm/ContentView.swift
@@ -20,55 +20,11 @@ struct ContentView: View {
             Group {
                 RealityView(arView: $viewModel.arView)
                     .onTapGesture { location in
-                        if viewModel.isRecording {
-                            stopRecording()
-                        } else {
-                            viewModel.tap(location: location)
-                        }
+                        viewModel.isRecording ? stopRecording() : viewModel.tap(location: location)
                     }
                 Group {
-                    ZStack {
-                        CanvasView(viewModel: viewModel, canvasView: $viewModel.canvasView, isCanvasVisible: $viewModel.isCanvasVisible, toolPicker: $viewModel.toolPicker)
-                            HStack {
-                                Spacer()
-                                Button {
-                                    viewModel.animationDrawings.append(viewModel.canvasView.drawing)
-                                    viewModel.canvasView.drawing = PKDrawing()
-                                } label: {
-                                    Image(systemName: "photo.badge.plus")
-                                        .font(.system(size: 30))
-                                        .foregroundColor(.accentColor)
-                                        .padding()
-                                        .background(
-                                            .ultraThinMaterial
-                                        )
-                                        .clipShape(.circle)
-                                }
-                                .opacity(viewModel.isCanvasBlank ? 0 : 1)
-                            }
-                        .padding(20)
-                        VStack {
-                            Spacer()
-                            HStack {
-                                Button {
-                                    viewModel.canvasView.drawing = PKDrawing()
-                                } label: {
-                                    Image(systemName: "rays")
-                                        .font(.system(size: 30))
-                                        .foregroundColor(.red)
-                                        .padding()
-                                        .background(
-                                            .ultraThinMaterial
-                                        )
-                                        .clipShape(.circle)
-                                }
-                                .opacity(viewModel.isCanvasBlank ? 0 : 1)
-                                Spacer()
-                            }
-                        }
-                        .padding(20)
-                    }
-                    .opacity(!viewModel.isCanvasVisible ? 0 : 1)
+                    canvasView
+                        .opacity(!viewModel.isCanvasVisible ? 0 : 1)
                     Image(uiImage: viewModel.drawingImage(canvasSize: true, drawing: viewModel.animationDrawings.isEmpty ? nil : viewModel.isCanvasBlank ? viewModel.animationDrawings.last! : nil))
                         .resizable()
                         .opacity(viewModel.isCanvasVisible || (viewModel.isCanvasBlank && viewModel.animationDrawings.isEmpty) ? 0 : 1)
@@ -80,104 +36,16 @@ struct ContentView: View {
             }
             .ignoresSafeArea()
             ZStack {
-                VStack {
-                    VStack {
-                        Text("\(Image(systemName: "hand.tap")) Tap screen to place drawing")
-                            .font(.system(size: 20, weight: .medium))
-                        HStack {
-                            Picker("Place on", selection: $viewModel.onPlane) {
-                                Image(systemName: "square.filled.on.square").tag(true)
-                                Image(systemName: "balloon").tag(false)
-                            }
-                            .pickerStyle(.segmented)
-                            .clipShape(RoundedRectangle(cornerRadius: 5, style: .circular))
-                            .frame(width: 300)
-                        }
-                    }
-                    .padding(20)
-                    .background(
-                        .ultraThinMaterial
-                    )
-                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                addView
                     .opacity(viewModel.isCanvasVisible || (viewModel.isCanvasBlank && viewModel.animationDrawings.isEmpty) ? 0 : 1)
-                    Spacer()
-                }
                 VStack {
-                    HStack {
-                        Button {
-                            viewModel.isCanvasVisible.toggle()
-                            viewModel.updateToolPicker()
-                            viewModel.tapSelectedEntity = nil
-                        } label: {
-                            Image(systemName: viewModel.isCanvasVisible ? "arkit" : "paintbrush")
-                                .font(.system(size: 30))
-                        }
-                        .padding()
-                        .background(
-                            .ultraThinMaterial
-                        )
-                        .clipShape(.circle)
-                        Spacer()
-                        if viewModel.tapSelectedEntity != nil {
-                            Button {
-                                viewModel.removeDrawing()
-                            } label: {
-                                Image(systemName: "trash")
-                                    .font(.system(size: 30))
-                                    .foregroundColor(.red)
-                                    .padding()
-                                    .background(
-                                        .ultraThinMaterial
-                                    )
-                                    .clipShape(.circle)
-                            }
-                        }
-                    }
+                    topButtons
                     Spacer()
                     historyCarousel
                         .opacity(viewModel.isCanvasVisible || viewModel.drawingHistory.isEmpty ? 0 : 1)
                 }
-                HStack {
-                    Spacer()
-                    VStack {
-                        Button {
-                            viewModel.takePicture()
-                            viewModel.tapSelectedEntity = nil
-                        } label: {
-                            Image(systemName: "camera.shutter.button.fill")
-                                .font(.system(size: 20))
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(
-                                    .ultraThinMaterial
-                                )
-                                .environment(\.colorScheme, .dark)
-                                .clipShape(.circle)
-                        }
-                        Button {
-                            isShowingRecordAlert = true
-                            viewModel.tapSelectedEntity = nil
-                        } label: {
-                            Image(systemName: "record.circle.fill")
-                                .font(.system(size: 20))
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(
-                                    .ultraThinMaterial
-                                )
-                                .environment(\.colorScheme, .dark)
-                                .clipShape(.circle)
-                        }
-                        .alert("Start Recording", isPresented: $isShowingRecordAlert) {
-                            Button("OK") {
-                                viewModel.startRecording()
-                            }
-                        } message: {
-                            Text("Tap screen to stop recording")
-                        }
-                    }
-                }
-                .opacity(viewModel.isCanvasVisible ? 0 : 1)
+                captureButtons
+                    .opacity(viewModel.isCanvasVisible ? 0 : 1)
             }
             .padding()
             .opacity(viewModel.isRecording ? 0 : 1)
@@ -187,6 +55,150 @@ struct ContentView: View {
                     .transition(.move(edge: .bottom))
                     .edgesIgnoringSafeArea(.all)
                 
+            }
+        }
+    }
+    
+    var canvasView: some View {
+        ZStack {
+            CanvasView(viewModel: viewModel, canvasView: $viewModel.canvasView, isCanvasVisible: $viewModel.isCanvasVisible, toolPicker: $viewModel.toolPicker)
+            HStack {
+                Spacer()
+                Button {
+                    viewModel.animationDrawings.append(viewModel.canvasView.drawing)
+                    viewModel.canvasView.drawing = PKDrawing()
+                } label: {
+                    Image(systemName: "photo.badge.plus")
+                        .font(.system(size: 30))
+                        .foregroundColor(.accentColor)
+                        .padding()
+                        .background(
+                            .ultraThinMaterial
+                        )
+                        .clipShape(.circle)
+                }
+                .opacity(viewModel.isCanvasBlank ? 0 : 1)
+            }
+            .padding(20)
+            VStack {
+                Spacer()
+                HStack {
+                    Button {
+                        viewModel.canvasView.drawing = PKDrawing()
+                    } label: {
+                        Image(systemName: "rays")
+                            .font(.system(size: 30))
+                            .foregroundColor(.red)
+                            .padding()
+                            .background(
+                                .ultraThinMaterial
+                            )
+                            .clipShape(.circle)
+                    }
+                    .opacity(viewModel.isCanvasBlank ? 0 : 1)
+                    Spacer()
+                }
+            }
+            .padding(20)
+        }
+    }
+    
+    var addView: some View {
+        VStack {
+            VStack {
+                Text("\(Image(systemName: "hand.tap")) Tap screen to place drawing")
+                    .font(.system(size: 20, weight: .medium))
+                HStack {
+                    Picker("Place on", selection: $viewModel.onPlane) {
+                        Image(systemName: "square.filled.on.square").tag(true)
+                        Image(systemName: "balloon").tag(false)
+                    }
+                    .pickerStyle(.segmented)
+                    .clipShape(RoundedRectangle(cornerRadius: 5, style: .circular))
+                    .frame(width: 300)
+                }
+            }
+            .padding(20)
+            .background(
+                .ultraThinMaterial
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            Spacer()
+        }
+    }
+    
+    var topButtons: some View {
+        HStack {
+            Button {
+                viewModel.isCanvasVisible.toggle()
+                viewModel.updateToolPicker()
+                viewModel.tapSelectedEntity = nil
+            } label: {
+                Image(systemName: viewModel.isCanvasVisible ? "arkit" : "paintbrush")
+                    .font(.system(size: 30))
+            }
+            .padding()
+            .background(
+                .ultraThinMaterial
+            )
+            .clipShape(.circle)
+            Spacer()
+            if viewModel.tapSelectedEntity != nil {
+                Button {
+                    viewModel.removeDrawing()
+                } label: {
+                    Image(systemName: "trash")
+                        .font(.system(size: 30))
+                        .foregroundColor(.red)
+                        .padding()
+                        .background(
+                            .ultraThinMaterial
+                        )
+                        .clipShape(.circle)
+                }
+            }
+        }
+    }
+    
+    var captureButtons: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Button {
+                    viewModel.takePicture()
+                    viewModel.tapSelectedEntity = nil
+                } label: {
+                    Image(systemName: "camera.shutter.button.fill")
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(
+                            .ultraThinMaterial
+                        )
+                        .environment(\.colorScheme, .dark)
+                        .clipShape(.circle)
+                }
+                Button {
+                    isShowingRecordAlert = true
+                    viewModel.tapSelectedEntity = nil
+                } label: {
+                    Image(systemName: "record.circle.fill")
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(
+                            .ultraThinMaterial
+                        )
+                        .environment(\.colorScheme, .dark)
+                        .clipShape(.circle)
+                }
+                .alert("Start Recording", isPresented: $isShowingRecordAlert) {
+                    Button("OK") {
+                        viewModel.startRecording()
+                    }
+                } message: {
+                    Text("Tap screen to stop recording")
+                }
             }
         }
     }
@@ -202,30 +214,30 @@ struct ContentView: View {
                         viewModel.animationDrawings = drawingDataArray.map { try! PKDrawing(data: $0) }.dropLast()
                         viewModel.tapSelectedEntity = nil
                     } label: {
-                            Image(uiImage: drawing.image(from: viewModel.canvasView.bounds, scale: 3))
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .background(
-                                    .ultraThinMaterial
-                                        .opacity(0.2)
-                                )
-                                .environment(\.colorScheme, .dark)
-                                .overlay {
-                                    if drawingDataArray.count > 1 {
-                                        VStack {
-                                            HStack {
-                                                Spacer()
-                                                Image(systemName: "film.circle.fill")
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fit)
-                                                    .frame(width: 25, height: 25)
-                                                    .padding(4)
-                                                    .foregroundStyle(.white)
-                                            }
+                        Image(uiImage: drawing.image(from: viewModel.canvasView.bounds, scale: 3))
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .background(
+                                .ultraThinMaterial
+                                    .opacity(0.2)
+                            )
+                            .environment(\.colorScheme, .dark)
+                            .overlay {
+                                if drawingDataArray.count > 1 {
+                                    VStack {
+                                        HStack {
                                             Spacer()
+                                            Image(systemName: "film.circle.fill")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 25, height: 25)
+                                                .padding(4)
+                                                .foregroundStyle(.white)
                                         }
+                                        Spacer()
                                     }
                                 }
+                            }
                     }
                     .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
                 }
